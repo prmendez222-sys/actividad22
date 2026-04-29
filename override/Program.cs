f282e1f1a0a83a82da60632a18bb722657073d7c
pedro mendez <prmendez222@gmail.com>
2026-04-28 22:20:52 -0600

    ejercicio 3

---
+++ override/Program.cs
@@ -0,0 +1,46 @@
+﻿Empleado e1 = new Empleado();
+e1.nombre = "juan";
+e1.TipoAcceso();
+Console.WriteLine();
+
+Visitante v1=new Visitante();
+v1.nombre = "ricardo";
+v1.TipoAcceso();
+Console.WriteLine();
+
+Seguridad s1 = new Seguridad();
+s1.nombre = "eliseo";
+s1.TipoAcceso();
+class Persona
+{
+    public string nombre;
+
+    public virtual void TipoAcceso()
+    {
+        Console.WriteLine(nombre+" tiene acceso basico");
+    }
+}
+
+class Empleado : Persona
+{
+    public override void TipoAcceso()
+    {
+        Console.WriteLine(nombre+" tiene acceso a oficinas");
+    }
+}
+
+class Visitante : Persona
+{
+    public override void TipoAcceso()
+    {
+        Console.WriteLine(nombre+"tiene acceso limitado");
+    }
+}
+
+class Seguridad : Persona
+{
+    public override void TipoAcceso()
+    {
+        Console.WriteLine(nombre+" tiene acceso al edificio");
+    }
+}
