04ec710bdb4f412b1ac4d70f8c288cfeb319e598
pedro mendez <prmendez222@gmail.com>
2026-04-28 20:09:34 -0600

    ejercicio 1 terminado

--- herencia_jerarquica/Program.cs
+++ herencia_jerarquica/Program.cs
@@ -1,7 +1,27 @@
-﻿class Persona
+﻿Estudiante e1 = new Estudiante();
+e1.nombre = "pedro";
+e1.correo = "prmendez@gmail.com";
+Console.WriteLine(e1.nombre); Console.WriteLine(e1.correo);
+e1.Iniciarsesion();
+e1.estudiar();
+Console.WriteLine();
+Profesor p1 = new Profesor();
+p1.nombre = "juan";
+p1.correo = "juan@gmail.com";
+Console.WriteLine(p1.nombre); Console.WriteLine(p1.correo);
+p1.Iniciarsesion();
+p1.ensenar();
+Console.WriteLine();
+Cordinador c1= new Cordinador();
+c1.nombre = "ricardo";
+c1.correo = "ricardo@gmail.com";
+Console.WriteLine(c1.nombre); Console.WriteLine(c1.correo);
+c1.Iniciarsesion();
+c1.supervisar();
+class Persona
 {
     public string nombre;
-    public DateOnly fecha_de_nacimiento;
+    public string correo;
 
     public void Iniciarsesion()
     {
@@ -11,6 +31,24 @@
 
 class Estudiante : Persona
 {
-
+    public void estudiar()
+    {
+        Console.WriteLine(nombre+" esta estudiando");
+    }
 }
 
+class Profesor: Persona
+{
+    public void ensenar()
+    {
+        Console.WriteLine(nombre+" esta ensenando");
+    }
+}
+
+class Cordinador : Persona
+{
+    public void supervisar()
+    {
+        Console.WriteLine(nombre + " esta supervisando");
+    }
+}
